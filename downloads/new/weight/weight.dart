77c66ad1758ff9f91fc2a861c7079416a60c86f5
40823201 <40823201@mde.tw>
2020-01-08 19:32:35 +0800

    w17 flag & weight & weight_change

--- downloads/new/weight/weight.dart
+++ downloads/new/weight/weight.dart
@@ -1,28 +1,39 @@
-lbtokg(num lb){
-return lb*0.4535;
+LBtoKG(num lb){
+
+return lb*0.45359237;
 }
 
-kgtolb(num kg){
-return kg*2.2046;
+
+KGtoLB(num kg){
+
+return kg*2.20462262;
 }
 
-main() { 
-  int len; 
-  var type;  
+
+main() {
+ 
+  int len;
+ 
+  var type;
+  
   var number;
-  List weight = ["25kg", "30lb", "56lb", "14kg", "68lb", "198kg"];
+  List weight = ["25K", "30L", "56L", "14K", "68L", "198K"];
   for (var data in weight) {
     
-    len = data.length;    
-    type = data[len-2];   
-    number = data.substring(0, len-2);   
+    len = data.length;
+    
+    type = data[len-1];
+   
+    number = data.substring(0, len-1);
+   
     number = int.parse(number);
    
-    if (type == "l"){     
-      print("$number 英磅 =  ${lbtokg(number)} 公斤");
-      
-    }else{     
-      print("$number 公斤 =  ${kgtolb(number)} 英磅");
+    if (type == "L"){
+     
+      print("$number 英磅 =  ${LBtoKG(number).toStringAsFixed(4)} 公斤");
+    }else{
+     
+      print("$number 公斤 =  ${KGtoLB(number).toStringAsFixed(4)} 英磅");
     }
       
   } 
